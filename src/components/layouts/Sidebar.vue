22bb862a929006607452c8b80f393f453722e871
Shan Yu Lin <ineslin35@gmail.com>
2020-11-21 07:52:46 +0800

    Edit sidebar

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -5,41 +5,38 @@
       class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
     >
       <div class="sidebar-sticky pt-3">
+        <h6
+          class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
+        >
+          <span>管理員</span>
+          <a
+            class="d-flex align-items-center text-muted"
+            href="#"
+            aria-label="Add a new report"
+          >
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
         <ul class="nav flex-column">
           <li class="nav-item">
             <a class="nav-link active" href="#">
               <span data-feather="home"></span>
-              Dashboard <span class="sr-only">(current)</span>
+              <i class="fas fa-list-ul"></i>
+              產品列表 <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
               <span data-feather="file"></span>
-              Orders
+              <i class="fas fa-shopping-cart"></i>
+              訂單列表
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
               <span data-feather="shopping-cart"></span>
-              Products
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users"></span>
-              Customers
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="bar-chart-2"></span>
-              Reports
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Integrations
+              <i class="fas fa-ticket-alt"></i>
+              優惠券
             </a>
           </li>
         </ul>
@@ -47,7 +44,7 @@
         <h6
           class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
         >
-          <span>Saved reports</span>
+          <span>模擬功能</span>
           <a
             class="d-flex align-items-center text-muted"
             href="#"
@@ -60,25 +57,8 @@
           <li class="nav-item">
             <a class="nav-link" href="#">
               <span data-feather="file-text"></span>
-              Current month
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Last quarter
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Social engagement
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Year-end sale
+              <i class="fas fa-store-alt"></i>
+              模擬訂單
             </a>
           </li>
         </ul>
