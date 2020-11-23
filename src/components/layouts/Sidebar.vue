3d8463ccff45d06faa8fe2074afb7a93ae13aeb8
Shan Yu Lin <ineslin35@gmail.com>
2020-11-23 19:24:38 +0800

    Added collapsable navbar

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>
+  <div class="nav-containr">
     <nav
+      class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow upper-nav"
+    >
+      <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#"
+        >Company name</a
+      >
+      <button
+        class="navbar-toggler position-absolute d-md-none collapsed"
+        type="button"
+        data-toggle="collapse"
+        data-target="#sidebarMenu"
+        aria-controls="sidebarMenu"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <input
+        class="form-control form-control-dark w-100"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#" @click.prevent="signout">登出</a>
+        </li>
+      </ul>
+    </nav>
+    <div>
+      <nav
       id="sidebarMenu"
       class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
     >
-      <button type="button" class="btn btn-success" @click.prevent="signout">登出</button>
       <div class="sidebar-sticky pt-3">
         <h6
           class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
@@ -65,16 +94,22 @@
         </ul>
       </div>
     </nav>
+    </div>
+    
   </div>
 </template>
+
+<style>
+.nav-containr {
+  width: 100%;
+}
+</style>
 
 <script>
 export default {
   name: "Sidebar",
   data() {
-    return {
-
-    };
+    return {};
   },
   methods: {
     signout() {
@@ -84,11 +119,10 @@ export default {
         console.log(response.data);
         if (response.data.success) {
           alert("您已登出");
-          location.href = '/login'
+          location.href = "/login";
         }
       });
     },
-  }
-
-}
+  },
+};
 </script>
